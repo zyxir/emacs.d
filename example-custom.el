0ab7cf2faad13f08ec0bf0a00f7a1f16c63207e4
Eric Zhuo Chen <zyxirchen@outlook.com>
2021-06-28 01:53:00 +0800

    custom file also changed

--- example-custom.el
+++ example-custom.el
@@ -3,3 +3,8 @@
 ;; Set the path of Zybox.
 (setq my/zybox-path "C:\\Zybox")	; Windows
 ;; (setq my/zybox-path "~/Zybox")		;Linux
+
+;; Set default font and size.
+(setq my/main-font-name "Sarasa Mono CL"
+      my/main-font-size 12)
+;; Default size is 11.
