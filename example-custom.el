87506d6a48c99cc4ad512c968fab735d240d3d13
Eric Zhuo Chen <zyxirchen@outlook.com>
2021-09-15 16:58:59 +0800

    sis integrated

--- example-custom.el
+++ example-custom.el
@@ -8,3 +8,8 @@
 (setq my/main-font-name "Sarasa Mono CL"
       my/main-font-size 12)
 ;; Default size is 11.
+
+;; Configure sis for specific platform.
+;; For example, on a GNU/Linux system with fcitx5 setup, the config would be:
+;; (with-eval-after-load "sis"
+;;   (sis-ism-lazyman-config "1" "2" 'fcitx5))
