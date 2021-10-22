88750d7116bb1d450575566864fe2a1464d03975
Eric Zhuo Chen <zyxirchen@outlook.com>
2021-10-22 16:04:57 +0800

    more latex customizations

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -8,17 +8,15 @@
 (use-package lsp-latex
   :straight t
   :defer t
+  :general
+  (:keymaps 'latex-mode-map
+	    "C-c l b" 'lsp-latex-build)
   :hook
   ((latex-mode yatex-mode bibtex-mode) .
    (lambda ()
+     (auto-fill-mode +1)
      (display-line-numbers-mode +1)
      (lsp))))
-
-(use-package latex
-  :after lsp-latex
-  :general
-  (:keymaps 'latex-mode-map
-	    "C-c l b" 'lsp-latex-build))
 
 ;; End of config.
 
