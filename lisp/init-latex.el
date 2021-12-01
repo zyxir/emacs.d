69d41c588d64fbeecc459613fd94e64d5c808cfe
Eric Zhuo Chen <zyxirchen@outlook.com>
2021-12-01 16:16:34 +0800

    close TeX compilation window on success

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -16,8 +16,27 @@
 				   t))
   (setq TeX-auto-save t
 	TeX-parse-self t
-	TeX-master nil
-	TeX-show-compilation t))
+	TeX-show-compilation t
+	TeX-command-force "LaTeX")
+  (setq-default TeX-master nil)
+  ;; From URL `https://emacs.stackexchange.com/questions/38258/close-latex-compilation-window-when-successful'
+  (setq TeX-buf-close-at-warnings-only t)
+  (defun my-tex-close-TeX-buffer (_output)
+    "Close compilation buffer if there are no errors.
+Hook this function into `TeX-after-compilation-finished-functions'."
+    (let ((buf (TeX-active-buffer)))
+      (when (buffer-live-p buf)
+	(with-current-buffer buf
+          (when (progn (TeX-parse-all-errors)
+                       (or
+			(and TeX-buf-close-at-warnings-only
+                             (null (cl-assoc 'error TeX-error-list)))
+			(null TeX-error-list)))
+            (cl-loop for win in (window-list)
+                     if (eq (window-buffer win) (current-buffer))
+                     do (delete-window win)))))))
+
+  (add-hook 'TeX-after-compilation-finished-functions #'my-tex-close-TeX-buffer))
 
 ;; End of config.
 
