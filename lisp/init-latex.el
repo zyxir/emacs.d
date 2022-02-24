5318f0a1ba95a6987022c52303226e464db44cbe
Eric Zhuo Chen <zyxirchen@outlook.com>
2022-02-24 09:50:08 +0800

    LaTeX settings updated (mainly synced preview)

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -17,8 +17,38 @@
   (setq TeX-auto-save t
 	TeX-parse-self t
 	TeX-show-compilation t
-	TeX-command-force "LaTeX")
-  (setq-default TeX-master nil)
+	TeX-command-force "LaTeX"
+	TeX-electric-escape t)
+  (setq-default TeX-master nil
+		font-latex-fontify-script nil)
+  ;; PDF Viewer.
+  (setq TeX-PDF-mode t)
+  (setq-default TeX-source-correlate-mode t)
+  (setq-default TeX-source-correlate-method 'synctex)
+  (when (executable-find "sumatrapdf")
+    (setq TeX-view-program-list
+	  '(("Sumatra PDF" ("\"path/to/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+			    (mode-io-correlate " -forward-search %b %n ") " %o"))))
+    (assq-delete-all 'output-pdf TeX-view-program-selection)
+    (add-to-list 'TeX-view-program-selection '(output-pdf "SumatraPDF")))
+  ;; Find master file automatically.
+  (defun TeX-find-master-file ()
+    "Finds the master file for TeX/LaTeX project by searching for
+  'main.tex' in the good directories"
+    (let (foundFiles (currPath (expand-file-name "./")) foundFile)
+      (while (or foundFiles (equal currPath "/"))
+	(setq foundFiles (directory-files currPath t "main\.tex"))
+	(setq currPath (expand-file-name (concat currPath "../"))))
+      (and
+       (setq foundFile (car foundFiles))
+       (setq foundFile (file-name-sans-extension foundFile))
+       (file-exists-p foundFile)
+       foundFile)))
+  (defun TeX-set-master-file (&optional ignore1 ignore2 ignore3)
+    "Finds the master file by means of TeX-find-master-file and
+  set TeX-master to it value"
+    (setq TeX-master (or (TeX-find-master-file) TeX-master)))
+  (add-hook 'TeX-mode-hook 'TeX-set-master-file)
   ;; From URL `https://emacs.stackexchange.com/questions/38258/close-latex-compilation-window-when-successful'
   (setq TeX-buf-close-at-warnings-only t)
   (defun my-tex-close-TeX-buffer (_output)
