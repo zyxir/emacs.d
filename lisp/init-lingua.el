c3a7220fc9dfd2385bc6e1b2da4cf50d0020a56c
Eric Zhuo Chen <zyxirchen@outlook.com>
2022-02-28 23:04:53 +0800

    Built-in Rime input method configured

--- lisp/init-lingua.el
+++ lisp/init-lingua.el
@@ -5,6 +5,36 @@
 
 ;;; Code:
 
+;; Built-in Rime input method.
+
+(defvar *builtin-rime* nil
+  "If built-in Rime is enabled.")
+(unless (boundp 'rime-user-data-dir)
+  (require 'cl-extra)
+  (let* ((rime-data-possible-locs-win64
+	  '("C:\\Users\\zyxir\\AppData\\Roaming\\Rime"))
+	 (rime-data-possible-locs-linux
+	  '("/mnt/c/Users/zyxir/AppData/Roaming/Rime"
+	    "~/.emacs.d/rime"))
+	 (guessed-rime-data
+	  (cl-some
+	   #'zy:file-directory-ret
+	   (cond
+	    (*win64* rime-data-possible-locs-win64)
+	    (*linux* rime-data-possible-locs-linux)))))
+    (if guessed-rime-data
+	(progn
+	  (setq rime-user-data-dir guessed-rime-data
+		*builtin-rime* t)
+	  (warn "Rime user data directory is auto detected."))
+      (warn "No Rime user data is detected, so Rime is unavailable"))))
+(when *builtin-rime*
+  (use-package rime
+    :straight t
+    :config
+    (setq default-input-method "rime"
+	  rime-show-candidate 'posframe)))
+
 ;; Smart input source.
 ;; Platform-specific settings should be included in custom.el.
 
