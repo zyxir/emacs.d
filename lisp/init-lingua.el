ae346c23552323d3378ec97c5ec62403c7d43cb6
Eric Zhuo Chen <zyxirchen@outlook.com>
2022-03-03 16:50:32 +0800

    Rime settings integrated into Emacs.

--- lisp/init-lingua.el
+++ lisp/init-lingua.el
@@ -7,40 +7,15 @@
 
 ;; Built-in Rime input method.
 
-(defvar *builtin-rime* nil
-  "If built-in Rime is enabled.")
-(unless (boundp 'rime-user-data-dir)
-  (require 'cl-extra)
-  (let* ((rime-data-possible-locs-win64
-	  '("C:\\Users\\zyxir\\AppData\\Roaming\\Rime"))
-	 (rime-data-possible-locs-linux
-	  '("/mnt/c/Users/zyxir/AppData/Roaming/Rime"
-	    "~/.emacs.d/rime"))
-	 (guessed-rime-data
-	  (cl-some
-	   #'zy:file-directory-ret
-	   (cond
-	    (*win64* rime-data-possible-locs-win64)
-	    (*linux* rime-data-possible-locs-linux)))))
-    (if guessed-rime-data
-	(progn
-	  (setq rime-user-data-dir guessed-rime-data
-		*builtin-rime* t))
-      (warn "No Rime user data is detected, so Rime is unavailable"))))
-(when *builtin-rime*
-  (use-package rime
-    :straight t
-    :config
-    (setq default-input-method "rime"
-	  rime-show-candidate 'posframe)))
-
-;; Smart input source.
-;; Platform-specific settings should be included in custom.el.
-
-(use-package sis
+(use-package rime
   :straight t
+  :general
+  ("C-`" 'rime-send-keybinding)
+  ("M-m" 'rime-inline-ascii)
   :config
-  (sis-global-respect-mode t))
+  (setq rime-user-data-dir (concat user-emacs-directory "rime")
+	default-input-method "rime"
+	rime-show-candidate 'posframe))
 
 ;; OpenCC.
 
