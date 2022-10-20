4a19fa3d2d913b95a10461149c1c612a6ff74faf
Eric Zhuo Chen <zyxirchen@outlook.com>
2022-10-20 09:35:25 +0800

    Move user snippets into etc

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -100,9 +100,18 @@ ARG is the arguments passed to OLDFUN."
 ;; Setup Yasnippet
 
 (straight-use-package 'yasnippet)
+(straight-use-package 'yasnippet-snippets)
+(straight-use-package 'consult-yasnippet)
 
 (zy/defsnip snip-yasnippet
     (:events 'find-file :weight 0)
+  (require 'yasnippet)
+  (require 'yasnippet-snippets)
+  (setq-default yas-snippet-dirs
+		(list
+		 (expand-file-name "etc/snippets"
+				   user-emacs-directory)
+		 yasnippet-snippets-dir))
   (yas-global-mode +1)
   ;; Do not auto expand snippet
   (zy/define-key :keymap 'yas-minor-mode-map
