bd95be222207dce46c8b32123861ccac30ce9dfa
Eric Zhuo Chen <zyxirchen@outlook.com>
2023-04-04 10:01:48 +0800

    Group configuration management code with Use-package

--- init.el
+++ init.el
@@ -2628,45 +2628,48 @@ Should be run again after theme switch."
 (declare-function restart-emacs 'restart-emacs)
 (declare-function restart-emacs-start-new-emacs 'restart-emacs)
 
-;; Open another Emacs to test the current config.
-(defun zy/test-config (&rest _)
-  "Start a new Emacs with \"--debug-init\" to test the config."
-  (interactive)
-  (restart-emacs-start-new-emacs '("--debug-init")))
+(use-package zy-conf-man
+  :defer t
+  :init
+  ;; Open another Emacs to test the current config.
+  (defun zy/test-config (&rest _)
+    "Start a new Emacs with \"--debug-init\" to test the config."
+    (interactive)
+    (restart-emacs-start-new-emacs '("--debug-init")))
 
-;; Open another Emacs with "-Q".
-(defun zy/test-no-site-file (&rest _)
-  "Start a new Emacs with \"-Q\"."
-  (interactive)
-  (restart-emacs-start-new-emacs '("-Q")))
+  ;; Open another Emacs with "-Q".
+  (defun zy/test-no-site-file (&rest _)
+    "Start a new Emacs with \"-Q\"."
+    (interactive)
+    (restart-emacs-start-new-emacs '("-Q")))
 
-;; Quickly open the config file.
-(defun zy/open-config (&rest _)
-  "Open Emacs configuration in another tab."
-  (interactive)
-  (let ((init-file (expand-file-name "init.el" user-emacs-directory)))
-    (find-file-other-tab init-file)))
+  ;; Quickly open the config file.
+  (defun zy/open-config (&rest _)
+    "Open Emacs configuration in another tab."
+    (interactive)
+    (let ((init-file (expand-file-name "init.el" user-emacs-directory)))
+      (find-file-other-tab init-file)))
 
-(defun zy--rebuild-config (&rest _)
-  "Repull all packages and rebuilt them."
-  (when (fboundp 'straight-pull-all)
-    (straight-pull-all))
-  (when (fboundp 'straight-rebuild-all)
-    (straight-rebuild-all)))
+  (defun zy--rebuild-config (&rest _)
+    "Repull all packages and rebuilt them."
+    (when (fboundp 'straight-pull-all)
+      (straight-pull-all))
+    (when (fboundp 'straight-rebuild-all)
+      (straight-rebuild-all)))
 
-(defun zy/rebuild-config (&rest _)
-  "Start a new Emacs and rebuild all packages."
-  (interactive)
-  (restart-emacs-start-new-emacs '("--debug-init"
-                                   "--eval"
-                                   "(zy--rebuild-config)")))
+  (defun zy/rebuild-config (&rest _)
+    "Start a new Emacs and rebuild all packages."
+    (interactive)
+    (restart-emacs-start-new-emacs '("--debug-init"
+                                     "--eval"
+                                     "(zy--rebuild-config)")))
 
-(general-def
-  "C-c e b" 'zy/rebuild-config
-  "C-c e e" 'zy/open-config
-  "C-c e t" 'zy/test-config
-  "C-c e q" 'zy/test-no-site-file
-  "C-c e R" 'restart-emacs)
+  (general-def
+    "C-c e b" 'zy/rebuild-config
+    "C-c e e" 'zy/open-config
+    "C-c e t" 'zy/test-config
+    "C-c e q" 'zy/test-no-site-file
+    "C-c e R" 'restart-emacs))
 
 ;;;;; Bibliography management
 
