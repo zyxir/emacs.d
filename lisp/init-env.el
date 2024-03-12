81c7766a8ba0b8283b48d9f300973eeb3bb0b09f
Eric Zhuo Chen <ericzhuochen@outlook.com>
2024-03-12 14:31:13 +0800

    Add editorconfig

--- lisp/init-env.el
+++ lisp/init-env.el
@@ -1,14 +1,18 @@
 ;;; init-env.el --- Programming environment.  -*- lexical-binding: t -*-
 ;;; Commentary:
 
-;; Load programming languages defined by Direnv and Nix.
+;; Load programming languages defined by Direnv, Nix, and other tools.
 
 ;;; Code:
 
 (require-package 'direnv)
+(require-package 'editorconfig)
 
 ;; Load Direnv mode.
 (direnv-mode 1)
+
+;; Load EditorConfig mode.
+(editorconfig-mode 1)
 
 (provide 'init-env)
 
