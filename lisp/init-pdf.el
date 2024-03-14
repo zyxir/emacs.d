74f8c164e021b04ac59aecde3c7f0935f1eda1a2
Eric Zhuo Chen <ericzhuochen@outlook.com>
2024-03-14 08:46:57 +0800

    Fix PDF blinking and other issues

--- lisp/init-pdf.el
+++ lisp/init-pdf.el
@@ -7,6 +7,16 @@
 ;; Load PDF Tools.
 (pdf-loader-install)
 
+(add-hook! pdf-view-mode
+  (setq-local evil-normal-state-cursor (list nil)))
+
+;; Silence "File *.pdf is large (X MiB), really open?" prompts for PDFs.
+(defadvice! zy/pdf-suppress-large-file-prompts-a
+  (fn size op-type filename &optional offer-raw)
+  :around #'abort-if-file-too-large
+  (unless (string-match-p "\\.pdf\\'" filename)
+    (funcall fn size op-type filename offer-raw)))
+
 (provide 'init-pdf)
 
 ;;; init-pdf.el ends here
