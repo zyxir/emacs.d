bc7bd882a235b43cfcbd038d301a9323619ec558
Eric Zhuo Chen <zyxirchen@outlook.com>
2024-03-14 14:55:29 +0800

    Incorporate Purcell's package refreshing utility

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -18,6 +18,28 @@
 (defvar required-packages '()
   "Packages required by `require-package'.")
 
+(defun require-package-symbol (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE.
+
+Do not use this directly. Use `require-package' instead."
+  (when (stringp min-version)
+    (setq min-version (version-to-list min-version)))
+  (or (package-installed-p package min-version)
+      (let* ((known (cdr (assoc package package-archive-contents)))
+             (best (car (sort known
+                              (lambda (a b)
+                                (version-list-<= (package-desc-version b)
+                                                 (package-desc-version a)))))))
+        (if (and best (version-list-<= min-version (package-desc-version best)))
+            (package-install best)
+          (if no-refresh
+              (error "No version of %s >= %S is available" package min-version)
+            (package-refresh-contents)
+            (require-package-symbol package min-version t)))
+        (package-installed-p package min-version))))
+
 (defun require-package (package &optional min-version)
   "Make sure that PACKAGE is installed with MIN-VERSION.
 
@@ -48,15 +70,7 @@ Anyway, if the package is installed, it is added to
       (cond
        ;; A package symbol.
        ((null spec-or-path)
-        (let* ((known (cdr (assoc package package-archive-contents)))
-               (best (car (sort known (lambda (a b)
-                                        (version-list-<=
-                                         (package-desc-version b)
-                                         (package-desc-version a)))))))
-          (if (and best (version-list-<= min-version
-                                         (package-desc-version best)))
-              (package-install best)
-            (error "No version of %s >= %S is available" package min-version))))
+        (require-package-symbol name min-version))
        ;; A VC package.
        (spec
         (package-vc-install package))
