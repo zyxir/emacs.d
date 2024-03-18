95307d48780ffc08c265e8d15685177cac30d9b7
Eric Zhuo Chen <ericzhuochen@outlook.com>
2024-03-18 11:11:55 +0800

    Tweak PDF settings

--- lisp/init-pdf.el
+++ lisp/init-pdf.el
@@ -7,12 +7,18 @@
 (require-package 'pdf-tools)
 
 ;; Load PDF Tools.
-(pdf-loader-install)
+(when (fboundp 'pdf-loader-install)
+  (pdf-loader-install))
 
+(after! 'pdf-tools
+  ;; Display PDF to fit page by default.
+  (setq-default pdf-view-display-size 'fit-page))
+
+;; HACK Fix page blinking while Evil is on.
 (add-hook! pdf-view-mode
   (setq-local evil-normal-state-cursor (list nil)))
 
-;; Silence "File *.pdf is large (X MiB), really open?" prompts for PDFs.
+;; HACK Silence "File *.pdf is large (X MiB), really open?" prompts for PDFs.
 (defadvice! zy/pdf-suppress-large-file-prompts-a
   (fn size op-type filename &optional offer-raw)
   :around #'abort-if-file-too-large
