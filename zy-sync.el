f5727250af52cf5f78290ee5ef53111cf8334fb1
Eric Zhuo Chen <ericzhuochen@outlook.com>
2024-03-27 16:49:41 +0800

    Use subprocess for synchronization

--- zy-sync.el
+++ zy-sync.el
@@ -6,6 +6,8 @@
 
 ;;; Code:
 
+(require 'comint)
+
 ;; Let Emacs ignore the "--sync" and "--force-sync" arguments.
 (add-to-list
  'command-line-functions
@@ -35,6 +37,49 @@
                      'full "^zy-.*\\.el$"))
   "List of available modules of Zyxir's Emacs configuration.")
 
+(defconst zy-sync-files (append zy-sync-zylib-components zy-sync-modules)
+  "List of files to synchronize.")
+
+(defvar zy-sync--proc nil
+  "The Emacs subprocess used for synchronization.")
+
+(defvar zy-sync--proc-buffer "*ZyEmacs-Sync*"
+  "The buffer name for the Emacs subprocess.")
+
+(defvar zy-sync--proc-init-form
+  `(progn
+     (package-initialize)
+     (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+     (add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
+     (require 'zylib)
+     (message "The Emacs subprocess is ready.")
+     (while t (eval (read-minibuffer ""))))
+  "Initialization form for the Emacs subprocess.")
+
+(defun zy-sync--proc-create ()
+  "Create and return the the Emacs subprocess."
+  (start-process "emacs" zy-sync--proc-buffer "emacs"
+                 "--batch" "--eval"
+                 (format "%S" zy-sync--proc-init-form)))
+
+(defun zy-sync-eval (form)
+  "Evaluate FORM in the Emacs subprocess.
+FORM is an s-expression."
+  (unless zy-sync--proc
+    (setq zy-sync--proc (zy-sync--proc-create)))
+  (let ((print-escape-newlines t)
+        (print-escape-control-characters t))
+    (process-send-string zy-sync--proc (format "%S\n" form))))
+
+(defun zy-sync-msg (format-string &rest args)
+  "Display a message in the Emacs subprocess.
+The message is formatted with FORMAT-STRING with ARGS."
+  (zy-sync-eval `(message ,(apply #'format format-string args))))
+
+(defun zy-sync-stop-proc ()
+  "Properly stop the Emacs subprocess."
+  (zy-sync-eval `(kill-emacs)))
+
 (defun zy-sync--read-sexp (buf)
   "Read one s-expression from buffer BUF.
 If no s-expression can be read, return nil."
@@ -42,41 +87,44 @@ If no s-expression can be read, return nil."
       (read buf)
     (error nil)))
 
-(defun zy-sync-ensure-pkgs-in (file)
-  "Ensure that all packages required by FILE is installed."
-  (require 'zylib-pkg)
-  (message "Ensuring packages required by %s..." file)
+(defun zy-sync--get-pkg-forms (file)
+  "Get all `pkg!' forms in FILE."
   (let* ((buf (find-file-noselect file))
-         (sexp nil))
+         (sexp nil)
+         (forms nil))
     (with-current-buffer buf
       (goto-char (point-min)))
     (while (setq sexp (zy-sync--read-sexp buf))
       (when (eq (car sexp) 'pkg!)
-        (eval sexp)
-        (message "  `%s' ensured." (cadr (cadr sexp)))))
-    (kill-buffer buf)))
+        (push sexp forms)))
+    (kill-buffer buf)
+    forms))
 
-(defun zy-sync-compile (file)
+(defun zy-sync-ensure-packages ()
+  "Ensure that all requested packages are installed."
+  (let ((pkg-forms (delete-dups
+                    (mapcan #'zy-sync--get-pkg-forms zy-sync-files))))
+    (zy-sync-msg "\n\nEnsuring packages...")
+    ;; Ensure all packages.
+    (dolist (form pkg-forms)
+      (zy-sync-eval form)
+      (when init-file-debug
+        (zy-sync-msg "  `%s' ensured." (cadr (cadr form)))))
+    (zy-sync-msg "%s packages ensured." (cl-list-length pkg-forms))
+    ;; Reload the quickstart file.
+    (zy-sync-msg "Refreshing the package quickstart file...")
+    (zy-sync-eval `(with-no-warnings
+                     (package-quickstart-refresh)))
+    (zy-sync-eval `(with-no-warnings
+                     (load package-quickstart-file nil 'nomessage)))))
+
+(defun zy-sync--compile (file)
   "Byte-compile (and maybe natively compile) FILE."
-  (let ((lisp-dir (expand-file-name "lisp" user-emacs-directory))
-        (modules-dir (expand-file-name "modules" user-emacs-directory)))
-    (message "Compiling %s..." file)
-    (call-process "emacs" nil "*ZyEmacs-Sync*" nil
-                  "--batch" "--eval"
-                  (format "
-(progn
-  (message \"Compiling %s...\")
-  (add-to-list 'load-path \"%s\")
-  (add-to-list 'load-path \"%s\")
-  (package-initialize)
-  (setq load-prefer-newer t)
-  (byte-compile-file \"%s\")
+  (zy-sync-eval `(byte-compile-file ,file))
   (when (and (fboundp 'native-comp-available-p)
              (native-comp-available-p))
-    (native-compile-async \"%s\"))
-  (message \"Compiling %s...done\"))
-" file lisp-dir modules-dir file file file))
-    (message "Compiling %s...done" file)))
+    (zy-sync-msg "Compiling %s..." file)
+    (zy-sync-eval `(native-compile-async ,file))))
 
 (defun zy-sync-maybe-compile (file &optional force)
   "Compile FILE if its byte code is outdated.
@@ -85,29 +133,44 @@ actually compiled, or nil otherwise."
   (let* ((compiled (concat file "c"))
          (outdated (file-newer-than-file-p file compiled)))
     (when (or force outdated)
-      (zy-sync-compile file)
+      (zy-sync--compile file)
       t)))
+
+(defun zy-sync-compile-all (&optional force)
+  "Re-compile every file.
+What files are re-compiled is decided smartly. If FORCE is
+non-nil, re-compile every file."
+  (zy-sync-msg "\n\nRe-compile everything%s"
+               (if force " forcefully..." "..."))
+  (let ((compiled-cnt 0))
+    ;; Try to compile Zylib. If any component of Zylib is outdated and
+    ;; re-compiled, everything else should be re-compiled as well, thus setting
+    ;; `force' to t.
+    (dolist (component zy-sync-zylib-components)
+      (when (zy-sync-maybe-compile component force)
+        (setq force t)
+        (setq compiled-cnt (+ compiled-cnt 1))))
+    ;; Now compile each module.
+    (dolist (module zy-sync-modules)
+      (when (zy-sync-maybe-compile module force)
+        (setq compiled-cnt (+ compiled-cnt 1))))
+    (zy-sync-msg "%s files re-compiled." compiled-cnt)))
 
 (defun zy-sync-sync (&optional force)
   "Do a synchronization of Zyxir's Emacs configuration.
 If FORCE is non-nil, force re-compile every file."
   (message "Synchronizing the configuration...")
-  ;; First, install all packages.
-  (dolist (file (append zy-sync-zylib-components zy-sync-modules))
-    (zy-sync-ensure-pkgs-in file))
-  ;; Second, refresh package cache to correctly autoload everything.
-  (package-quickstart-refresh)
-  (load package-quickstart-file 'noerror 'nomessage)
-  ;; Third, try to compile Zylib. If any component of Zylib is compiled,
-  ;; everything should be re-compiled, thus setting `force' to t.
-  (dolist (component zy-sync-zylib-components)
-    (when (zy-sync-maybe-compile component force)
-      (setq force t)))
-  ;; Finally, compile each module if: (a) `force' is non-nil, (b) it does not
-  ;; have bytecode, or (c) its byte code is outdated.
-  (dolist (module zy-sync-modules)
-    (zy-sync-maybe-compile module force))
-  (message "Synchronizing the configuration...done"))
+  (message "See buffer %s for details" zy-sync--proc-buffer)
+  (let* ((result
+          (benchmark-run 1
+            ;; Make sure all packages are installed.
+            (zy-sync-ensure-packages)
+            ;; Re-compile everything smartly.
+            (zy-sync-compile-all force)
+            ;; Kill the process.
+            (zy-sync-stop-proc)))
+         (secs (car result)))
+  (message "Synchronizing completed in %s seconds." secs)))
 
 ;; Everything needed has been defined. Synchronize now!
 (zy-sync-sync (bound-and-true-p zy-sync-force))
