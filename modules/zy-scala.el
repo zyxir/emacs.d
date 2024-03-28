ec9c161637f53dffc9adb9f36d9ed9cc0d1f07c8
Eric Zhuo Chen <ericzhuochen@outlook.com>
2024-03-28 18:10:25 +0800

    Tweak Scala commands and indentation

--- modules/zy-scala.el
+++ modules/zy-scala.el
@@ -17,6 +17,46 @@
 (add-hook! 'scala-ts-mode-hook
   (setq-local fill-column 100))
 
+(after! '(scala-ts-mode sbt-mode)
+  ;; Indent 4 spaces for function arguments and class parameters, as the scala
+  ;; formatter does.
+  (setf (alist-get
+         '(parent-is "^class_parameters$")
+         (alist-get 'scala scala-ts--indent-rules)
+         nil nil #'equal)
+        '(parent-bol 4)
+        (alist-get
+         '(parent-is "^arguments$")
+         (alist-get 'scala scala-ts--indent-rules)
+         nil nil #'equal)
+        '(parent-bol 4)
+        (alist-get
+         '(parent-is "^parameters$")
+         (alist-get 'scala scala-ts--indent-rules)
+         nil nil #'equal)
+        '(parent-bol 4))
+
+  ;; Command to start and switch to the sbt shell.
+  (eval-and-compile
+    (defun +scala-switch-to-sbt-shell ()
+      "Switch to the sbt buffer.
+When there is no sbt process running, start one before
+switching to it."
+      (interactive)
+      (unless (and (get-buffer (sbt:buffer-name))
+                   (get-buffer-process (sbt:buffer-name)))
+        (sbt:run-sbt nil nil))
+      (sbt-switch-to-active-sbt-buffer)))
+
+  (defprefix! +scala-map "Scala"
+              nil scala-ts-mode-map "<localleader>")
+
+  (defprefix! +scala-s-map "Sbt"
+              nil +scala-map "s"
+    "s" (cons "Shell" #'+scala-switch-to-sbt-shell)
+    "c" (cons "Command" #'sbt-command)
+    "C" (cons "Run Last Command" #'sbt-run-previous-command)))
+
 (provide 'zy-scala)
 
 ;;; zy-scala.el ends here
