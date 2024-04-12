edff3b9c9e29d0174492b739928c7c1af9836a4f
Eric Zhuo Chen <zyxirchen@outlook.com>
2024-04-12 16:13:46 +0800

    Do not prettify window dividers on terminal since it's unstable

--- modules/zy-terminal.el
+++ modules/zy-terminal.el
@@ -39,11 +39,6 @@
 (pkg! 'evil-terminal-cursor-changer)
 (pkg! 'corfu-terminal)
 
-;; Use a prettier character to divide windows.
-(unless standard-display-table
-  (setq standard-display-table (make-display-table)))
-(set-display-table-slot standard-display-table 5 #x2502)
-
 ;; Enable mouse clicks in the terminal.
 (xterm-mouse-mode 1)
 
